070e9cbff27975eda34a694a646d61895147be62
Huangkexin <592125471@qq.com>
2024-06-06 01:10:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def send_message():
         return jsonify({'reply': None, 'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
