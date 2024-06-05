051935b0ddb3f88d72c7c1344abb9bdc485ac63d
Huangkexin <592125471@qq.com>
2024-06-06 00:44:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-OPENAI_API_KEY = ''
+OPENAI_API_KEY = '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'
 ASSISTANT_ID = 'asst_JRXuMN50S3EbVllQmhN9sNMA' # You can find this in your platform.openai.com
